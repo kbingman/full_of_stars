f04cdad083ddc2cda443ca3377966ba06fddf1c4
Keith Bingman <keith@keithbingman.com>
2012-05-14 18:00:16 +0200

    attempt to hook up flatiron

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // My SocketStream app
 
-var http = require('http'),
+var flatiron = require('flatiron'),
+    path = require('path'),
+    app = flatiron.app,
     ss = require('socketstream'),
     resourceful = require('resourceful-mongo');
+    
+app.use(flatiron.plugins.http);
+app.http.before.push(ss.http.middleware);
         
 // Define a single-page client
 ss.client.define('main', {
@@ -23,14 +28,18 @@ ss.client.define('admin', {
 });
 
 // Serve this client on the root URL
-ss.http.route('/', function(req, res){
-  res.serveClient('main');
+app.router.get('/', function () {
+  console.log(ss.http.middleware)
+  this.res.json({ 'hello': 'world' });
+  // this.res.serveClient('main');
 });
 
+
 // Serve this client on the admin URL
-ss.http.route('/admin', function(req, res){
-  res.serveClient('admin');
+app.router.get('/', function () {
+  this.res.serveClient('admin');
 });
+
 
 // Code Formatters
 // ss.client.formatters.add(require('ss-stylus'));
@@ -60,8 +69,8 @@ if (ss.env == 'production'){
 }
 
 // Start web server
-var server = http.Server(ss.http.middleware);
-server.listen(3000);
+
+app.start(3000);
 
 
 // Open the mongodb connection
@@ -71,7 +80,7 @@ resourceful.use('mongodb', {
     // Start SocketStream
     if(!err){
       console.log('connected')
-      ss.start(server);
+      ss.start(app);
     }
   }
 });
