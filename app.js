d70fae86f146f4f60b339bcec1dd685304755ffc
Keith Bingman <keith@keithbingman.com>
2012-05-17 14:06:23 +0200

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var http = require('http'),
     ss = require('socketstream'),
     resourceful = require('resourceful-mongo');
+    
         
 // Define a single-page client
 ss.client.define('main', {
   view: 'app.html',
   css:  ['libs/bootstrap', 'index.css'],
-  code: ['libs/jquery.min.js', 'libs/bootstrap.js', 'app'],
+  code: ['libs/jquery.min.js', 'libs/bootstrap.js', 'libs/sugar.min.js', 'app'],
   system: '*',
   tmpl: '*'
 });
@@ -14,16 +15,18 @@ ss.client.define('main', {
 // Define a single-page client
 ss.client.define('admin', {
   view: 'admin.html',
-  css:  ['libs/bootstrap', '/admin'],
-  code: ['libs/jquery.min.js', 'libs/bootstrap.js', 'admin', 'system'],
+  css:  ['libs/bootstrap/bootstrap.css','libs/bootstrap/bootstrap-responsive.css', 'admin'],
+  code: ['libs/jquery.min.js', 'libs/bootstrap.js', 'libs/sugar.min.js', 'admin', 'system'],
   system: ['models/weapons.js'],
   tmpl: '*'
 });
 
-// Serve this client on the root URL
+
+// Serve this client on the admin URL
 ss.http.route('/', function(req, res){
   res.serveClient('main');
 });
+
 
 // Serve this client on the admin URL
 ss.http.route('/admin', function(req, res){
@@ -32,29 +35,32 @@ ss.http.route('/admin', function(req, res){
 
 // Code Formatters
 ss.client.formatters.add(require('ss-less'));
+// Use server-side compiled Hogan (Mustache) templates. Others engines available
 ss.client.templateEngine.use(require('ss-hogan'));
 
-// Production settings: SS_ENV=production node app.js
+// Minimize and pack assets if you type: SS_ENV=production node app.js
 if (ss.env == 'production'){
   ss.client.packAssets();
-  var redisCredentials =  {
+  ss.session.store.use('redis', {
     host: 'panga.redistogo.com', 
     port: 9538, 
     pass: '351ce528621de5837d0f6c7828789ad2'
-  }
-  ss.session.store.use('redis', redisCredentials);
-  ss.publish.transport.use('redis', redisCredentials);
+  });
+  ss.publish.transport.use('redis', {
+    host: 'panga.redistogo.com', 
+    port: 9538, 
+    pass: '351ce528621de5837d0f6c7828789ad2'
+  });
   resourceful.db = 'mongodb://fullofstars:123456@staff.mongohq.com:10057/fullOfStars';
-} else if (ss.env == 'development') {
-  resourceful.db = 'mongodb://localhost/planetary'; 
 } else {
-  resourceful.db = 'mongodb://localhost/planetary_test'; 
+  resourceful.db = 'mongodb://localhost/planetary'; 
 }
 
 // Open the mongodb connection
 resourceful.use('mongodb', {
-  uri: resourceful.db, 
-  onConnect: function (err) { 
+  uri: resourceful.db, // required - the connection to be opened
+  onConnect: function (err) { // required - the callback upon opening the database connection
+    // Start SocketStream
     if(!err){
       console.log('connected')
       // Start web server
